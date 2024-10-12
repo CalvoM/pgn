d2aee51866f021ba11e354772d73ce4bcd0d2dc0
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2024-10-12 17:58:54 +0300

    views: return games based on filters from url params.

--- past_chess/views.py
+++ past_chess/views.py
@@ -1,10 +1,14 @@
 # pyright:reportUnnecessaryComparison=false,reportArgumentType=false
+import json
+
 from celery.result import AsyncResult
 from django import forms
+from django.core.serializers import serialize
 from django.http import HttpRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_POST
 
+from past_chess.models import Game, ModelJsonSerializer
 from pgn_web import celery_app
 
 from .forms import UploadFileForm
@@ -32,7 +36,36 @@ def check_upload_status(request: HttpRequest, task_id: str):
 
 
 @csrf_exempt
-@require_POST
+@require_GET
 def filter_gamees(request: HttpRequest):
-    print(request.body)
-    return JsonResponse([1, 2, 3], safe=False)
+    findings = Game.objects.all()
+    white_player = request.GET.get("white")
+    if white_player:
+        findings = findings.filter(white__contains=white_player)
+    black_player = request.GET.get("black")
+    if black_player:
+        findings = findings.filter(black__contains=black_player)
+    event = request.GET.get("event")
+    if event:
+        findings = findings.filter(event__contains=event)
+    site = request.GET.get("site")
+    if site:
+        findings = findings.filter(site__contains=site)
+    date = request.GET.get("date")
+    if date:
+        findings = findings.filter(date__date=date)
+    round = request.GET.get("round")
+    if round:
+        findings = findings.filter(round=round)
+    result = request.GET.get("result")
+    if result:
+        findings = findings.filter(result=result)
+
+    serialize
+    ret = ModelJsonSerializer().serialize(
+        queryset=findings,
+        fields=("event", "site", "white", "black"),
+        use_natural_foreign_keys=True,
+        use_natural_primary_keys=True,
+    )
+    return JsonResponse(json.loads(ret), safe=False)
